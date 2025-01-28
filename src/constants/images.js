cfc11097ccf513f8ebe73babe7e0c07d17f6a016
satyam9798 <satyamshivam9798@gmail.com>
2025-01-28 20:57:26 +0530

    Updated: images constants

---
+++ src/constants/images.js
@@ -0,0 +1,23 @@
+import Expertise from "../assets/services/expertise.png";
+import Commitment from "../assets/services/commitment.png";
+import Proven from "../assets/services/proven.png";
+import Competitive from "../assets/services/competitive.png";
+import Tata from "../assets/services/tata.png";
+import Adani from "../assets/services/adani.png";
+import Waaree from "../assets/services/waaree.png";
+import Loom from "../assets/services/loom.png";
+import ServiceArea from "../assets/services/serviceArea.png";
+import TeamExpertise from "../assets/services/teamExpertise.png";
+
+export {
+  Expertise,
+  Commitment,
+  Proven,
+  Competitive,
+  Tata,
+  Adani,
+  Waaree,
+  Loom,
+  ServiceArea,
+  TeamExpertise,
+};
