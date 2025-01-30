2f5d9669e65565ac9d48660b912b92017b521043
satyam9798 <satyamshivam9798@gmail.com>
2025-01-30 20:44:47 +0530

    Added: Images

--- src/constants/images.js
+++ src/constants/images.js
@@ -8,6 +8,10 @@ import Waaree from "../assets/services/waaree.png";
 import Loom from "../assets/services/loom.png";
 import ServiceArea from "../assets/services/serviceArea.png";
 import TeamExpertise from "../assets/services/teamExpertise.png";
+import InstallationExample from '../assets/installation_ex.png';
+import RooftopFeature from '../assets/rooftop_feature.png';
+import RooftopSolarExample from '../assets/rooftop_solar_ex.jpg';
+import SolarZoom from '../assets/solar_zoom.jpg';
 
 export {
   Expertise,
@@ -20,4 +24,8 @@ export {
   Loom,
   ServiceArea,
   TeamExpertise,
+  InstallationExample,
+  RooftopFeature,
+  RooftopSolarExample,
+  SolarZoom
 };
